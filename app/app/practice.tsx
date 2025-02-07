a052e8a7b999d281cae9143c91f4d29634599940
wslyvh <wslyvh@users.noreply.github.com>
2025-02-07 16:40:03 +0100

    permissions fix

--- app/app/practice.tsx
+++ app/app/practice.tsx
@@ -1,15 +1,12 @@
 import { ScreenLayout } from "@/components/screen-layout";
 import { PracticeTimer } from "@/components/practice/timer";
 import { useRecorder } from "@/context/recording";
-import { Permissions } from "@/components/permissions";
 
 export default function Practice() {
   const recorder = useRecorder();
 
   return (
     <ScreenLayout title="Practice Timer" cancelable={recorder.state !== ""}>
-      <Permissions className="my-4" />
-
       <PracticeTimer />
     </ScreenLayout>
   );
