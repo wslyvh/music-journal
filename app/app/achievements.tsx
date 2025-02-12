4d0a602f300097a6c4ef790c189e44672e811a42
wslyvh <wslyvh@users.noreply.github.com>
2025-02-12 12:09:58 +0100

    stats spacing

--- app/app/achievements.tsx
+++ app/app/achievements.tsx
@@ -28,44 +28,52 @@ export default function Achievements() {
     <ScreenLayout title="Achievements">
       <View className="flex flex-row gap-4 justify-between mb-4">
         <View className="flex flex-col bg-base-200 rounded-xl p-4 gap-2 flex-1">
+          <View className="flex-row items-baseline pt-2">
           <Text className="text-4xl font-bold text-base-content">
             {stats.total}
           </Text>
+          </View>
           <Text className="text-muted">Total sessions</Text>
         </View>
 
         <View className="flex flex-col bg-base-200 rounded-xl p-4 gap-2 flex-1">
+          <View className="flex-row items-baseline pt-2">
           <Text className="text-4xl font-bold text-base-content">
             {formatDuration(stats.totalDuration)}
-            <Text className="text-2xl text-muted">
+            </Text>
+            <Text className="text-2xl text-muted ml-1">
               {stats.totalDuration < 7200 && " min"}
               {stats.totalDuration >= 7200 && " hr"}
             </Text>
-          </Text>
+          </View>
           <Text className="text-muted">Total practice time</Text>
         </View>
       </View>
 
       <View className="flex flex-row gap-4 justify-between mb-8">
         <View className="flex flex-col bg-base-200 rounded-xl p-4 gap-2 flex-1">
+          <View className="flex-row items-baseline pt-2">
           <Text className="text-4xl font-bold text-base-content">
             {formatDuration(stats.averageSession)}
-            <Text className="text-2xl text-muted">
+            </Text>
+            <Text className="text-2xl text-muted ml-1">
               {stats.averageSession > 7200 && " hr"}
               {stats.averageSession < 7200 && " min"}
             </Text>
-          </Text>
+          </View>
           <Text className="text-muted">Average session</Text>
         </View>
 
         <View className="flex flex-col bg-base-200 rounded-xl p-4 gap-2 flex-1">
+          <View className="flex-row items-baseline pt-2">
           <Text className="text-4xl font-bold text-base-content">
             {formatDuration(stats.longestSession)}
-            <Text className="text-2xl text-muted">
+            </Text>
+            <Text className="text-2xl text-muted ml-1">
               {stats.longestSession > 7200 && " hr"}
               {stats.longestSession < 7200 && " min"}
             </Text>
-          </Text>
+          </View>
           <Text className="text-muted">Longest session</Text>
         </View>
       </View>
--- app/components/practice/overview.tsx
+++ app/components/practice/overview.tsx
@@ -120,29 +120,35 @@ export function PracticeOverview(props: Props) {
       <View className={className}>
         <View className="flex flex-row gap-4 justify-between mb-4">
           <View className="flex flex-col bg-base-200 rounded-xl p-4 gap-2 flex-1">
+            <View className="flex-row items-baseline pt-2">
             <Text className="text-4xl font-bold text-base-content">
               {stats.total}
             </Text>
+            </View>
             <Text className="text-muted">Total sessions</Text>
           </View>
 
           <View className="flex flex-col bg-base-200 rounded-xl p-4 gap-2 flex-1">
+            <View className="flex-row items-baseline pt-2">
             <Text className="text-4xl font-bold text-base-content">
               {formatDuration(stats.totalDuration)}
-              <Text className="text-2xl text-muted">
+              </Text>
+              <Text className="text-2xl text-muted ml-1">
                 {stats.totalDuration < 7200 && " min"}
                 {stats.totalDuration >= 7200 && " hr"}
               </Text>
-            </Text>
+            </View>
             <Text className="text-muted">Total practice time</Text>
           </View>
         </View>
 
         <View className="bg-base-200 rounded-xl p-4 mb-4 gap-2">
+          <View className="flex-row items-baseline pt-2">
           <Text className="text-xl font-bold text-base-content">
-            Lastest activity
+              Latest activity
           </Text>
-          <Text className="text-muted">Past week</Text>
+          </View>
+          <Text className="text-muted">Current streak</Text>
 
           <View className="flex flex-row mt-4">
             {[7, 6, 5, 4, 3, 2, 1, 0].map((i) => {
