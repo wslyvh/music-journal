804f50d06400592610f9c7f80cd9af619c836283
wslyvh <wslyvh@users.noreply.github.com>
2025-02-12 12:12:37 +0100

    wrap

--- app/app/achievements.tsx
+++ app/app/achievements.tsx
@@ -29,17 +29,17 @@ export default function Achievements() {
       <View className="flex flex-row gap-4 justify-between mb-4">
         <View className="flex flex-col bg-base-200 rounded-xl p-4 gap-2 flex-1">
           <View className="flex-row items-baseline pt-2">
-          <Text className="text-4xl font-bold text-base-content">
-            {stats.total}
-          </Text>
+            <Text className="text-4xl font-bold text-base-content">
+              {stats.total}
+            </Text>
           </View>
           <Text className="text-muted">Total sessions</Text>
         </View>
 
         <View className="flex flex-col bg-base-200 rounded-xl p-4 gap-2 flex-1">
           <View className="flex-row items-baseline pt-2">
-          <Text className="text-4xl font-bold text-base-content">
-            {formatDuration(stats.totalDuration)}
+            <Text className="text-4xl font-bold text-base-content">
+              {formatDuration(stats.totalDuration)}
             </Text>
             <Text className="text-2xl text-muted ml-1">
               {stats.totalDuration < 7200 && " min"}
@@ -53,8 +53,8 @@ export default function Achievements() {
       <View className="flex flex-row gap-4 justify-between mb-8">
         <View className="flex flex-col bg-base-200 rounded-xl p-4 gap-2 flex-1">
           <View className="flex-row items-baseline pt-2">
-          <Text className="text-4xl font-bold text-base-content">
-            {formatDuration(stats.averageSession)}
+            <Text className="text-4xl font-bold text-base-content">
+              {formatDuration(stats.averageSession)}
             </Text>
             <Text className="text-2xl text-muted ml-1">
               {stats.averageSession > 7200 && " hr"}
@@ -66,8 +66,8 @@ export default function Achievements() {
 
         <View className="flex flex-col bg-base-200 rounded-xl p-4 gap-2 flex-1">
           <View className="flex-row items-baseline pt-2">
-          <Text className="text-4xl font-bold text-base-content">
-            {formatDuration(stats.longestSession)}
+            <Text className="text-4xl font-bold text-base-content">
+              {formatDuration(stats.longestSession)}
             </Text>
             <Text className="text-2xl text-muted ml-1">
               {stats.longestSession > 7200 && " hr"}
@@ -135,9 +135,9 @@ export default function Achievements() {
               const nextWeek = date.add(1, "week");
 
               return (
-                <View key={`month-${weekIndex}`} className="flex-1">
+                <View key={`month-${weekIndex}`} className="flex-1 relative">
                   {date.format("MMM") !== nextWeek.format("MMM") && (
-                    <Text className="text-xs absolute left-0">
+                    <Text className="text-xs absolute left-0 w-8">
                       {nextWeek.format("MMM")}
                     </Text>
                   )}
